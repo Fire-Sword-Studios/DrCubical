98b7f3c2e5a6a11eb36081f20859f8593235d7ea
leoTigers <jimenez.lorenzo@orange.fr>
2021-08-20 11:39:38 +0200

    prompt user values for .env vars

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Install required npm modules
 npm install
-cp examples/.env .env
 
+FILE=.env
+EXAMPLE_FILE=examples/.env
+
+# Create .env file
+touch "$FILE"
+
+# Prompt user for .env values
+while IFS="" read -r -u9 p || [ -n "$p" ]
+do
+  read -r -e -p "${p: : -1}" input
+  echo "${p}${input}" >> "$FILE"
+done 9< "$EXAMPLE_FILE"
