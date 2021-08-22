ea6fd3ba3bb28bcee92ec24be0e536f0b049ceaf
leoTigers <jimenez.lorenzo@orange.fr>
2021-08-22 22:06:58 +0200

    pat command

---
+++ commands/pat.js
@@ -0,0 +1,87 @@
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const fs = require('fs');
+const fetch = require('node-fetch');
+const Jimp = require('jimp');
+const {BitmapImage, GifFrame, GifUtil} = require('gifwrap');
+const sharp = require('sharp');
+
+/**
+ * download a file from an url
+ * @param {str} url Url to download from
+ * @param {str} path Path to save the image
+ * @param {function} callback Function to execute once download is complete
+ */
+async function download(url, path, callback) {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  fs.writeFile(path, buffer, callback);
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('pat')
+      .setDescription('Pat the person')
+      .addUserOption((option) =>
+        option.setName('target')
+            .setDescription('The person you want to pat')
+            .setRequired(true)),
+  async execute(interaction) {
+    // Ack the interaction
+    interaction.deferReply();
+
+    // Download and save the image
+    const target = interaction.options.getUser('target');
+    const avatarURL = target.avatarURL();
+    const fileExtention = avatarURL.split('.').pop();
+    const filename = `./tmp/${target.username}.${fileExtention}`;
+    const filenamePNG = `./tmp/${target.username}.png`;
+    await download(avatarURL, filename, () => {
+      console.log('wrote');
+    });
+
+    // Convert the image to png, Jimp can't handle WebP
+    await sharp(filename)
+        .png()
+        .toFile(filenamePNG);
+
+    const avatar = await Jimp.read(filenamePNG);
+    const spriteTemplate = await Jimp.read('./img/pat.png');
+    const outputPath = `./tmp/pat.gif`;
+    const frames = [];
+    const AVATAR_CRUNCH_SIZES = [
+      [112, 112],
+      [112, 87],
+      [112, 78],
+      [112, 87],
+      [112, 100],
+    ];
+
+    // Create the gif
+    for (let i=0; i<5; i++) {
+      const tmp = new Jimp(112, 112, 0x0, (err, image) =>{});
+      const crunchedAvatar = avatar.clone()
+          .resize(AVATAR_CRUNCH_SIZES[i][0], AVATAR_CRUNCH_SIZES[i][1]);
+      tmp.blit(crunchedAvatar, 0, 112-AVATAR_CRUNCH_SIZES[i][1]);
+      tmp.blit(spriteTemplate, 0, 0, 112*i, 0, 112, 112);
+      const bitmap = new BitmapImage(tmp.bitmap);
+      // Limit to 256 colors (gif limitation)
+      GifUtil.quantizeWu(bitmap, 256);
+      frames.push(new GifFrame(
+          bitmap,
+          {delayCentisecs: 6}));
+    }
+    await GifUtil.write(outputPath, frames);
+
+    // Reply using the created gif
+    await interaction.editReply({
+      content: `<@${target.id}> got pats`,
+      files: [outputPath],
+    });
+    const wait = require('util').promisify(setTimeout);
+    await wait(60000);
+    // Clean files
+    fs.unlinkSync(outputPath);
+    fs.unlinkSync(filenamePNG);
+    fs.unlinkSync(filename);
+  },
+};
